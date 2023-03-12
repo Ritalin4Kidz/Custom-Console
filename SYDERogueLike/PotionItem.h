7607afa7bbf6a5e79dc9c191d8e5802c7284f271
Callum Hands <callumhands@ordermate.com.au>
2023-03-12 12:06:10 +1100

    Potion Animation

--- SYDERogueLike/PotionItem.h
+++ SYDERogueLike/PotionItem.h
@@ -5,10 +5,10 @@ class PotionItem : public ConsumableItem
 public:
 	PotionItem()
 	{
-		m_ItemIcon = CustomAsset_Clickable(16, 8, AssetsClass::get_bmp_as_direct_colour_class_array(L"EngineFiles\\Bitmaps\\ItemIcons\\TestItem.bmp", 8, 8));
+		m_ItemIcon = CustomAsset_Clickable(16, 8, AssetsClass::get_bmp_as_direct_colour_class_array(L"EngineFiles\\Bitmaps\\ItemIcons\\Potion.bmp", 8, 8));
 		m_Name = "A Potion";
 		m_Type = Typeless;
-		m_Animation.setAsset(AnimationSpriteSheets::load_from_animation_sheet(L"EngineFiles\\Animations\\AttackAnimations\\Electric\\LightningPunch.bmp", 100, 100, 20, 20, 0, 25));
+		m_Animation.setAsset(AnimationSpriteSheets::load_from_animation_sheet(L"EngineFiles\\Animations\\ItemUseAnimations\\PotionUsage.bmp", 100, 180, 20, 20, 0, 45));
 	}
 	void Execute(json* Attacker, json* Defender, std::string* tag) override;
 private:

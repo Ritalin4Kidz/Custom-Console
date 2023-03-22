289d284a726d1901189ea553fc306d3fd06b8f1b
Callum Hands <callumhands@ordermate.com.au>
2023-03-22 16:30:28 +1100

    Update CrabClawMove.h

--- SYDERogueLike/CrabClawMove.h
+++ SYDERogueLike/CrabClawMove.h
@@ -7,7 +7,7 @@ public:
 		m_Name = "Crab Claw";
 		m_Type = Melee;
 		BasePower = 35;
-		m_Animation.setAsset(AnimationSpriteSheets::load_from_animation_sheet(L"EngineFiles\\Animations\\AttackAnimations\\Melee\\BoneAttack.bmp", 100, 100, 20, 20, 0, 25));
+		m_Animation.setAsset(AnimationSpriteSheets::load_from_animation_sheet(L"EngineFiles\\Animations\\AttackAnimations\\Melee\\CrabAttack.bmp", 100, 100, 20, 20, 0, 25));
 		maxUsages = 25;
 		usagesLeft = maxUsages;
 	}

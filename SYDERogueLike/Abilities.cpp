fa71182565067b929acc5624a5df4fbd20e59bef
Callum Hands <callumhands@ordermate.com.au>
2023-03-22 16:22:02 +1100

    Fix

---
+++ SYDERogueLike/Abilities.cpp
@@ -0,0 +1,8 @@
+#include "Abilities.h"
+AbilityStruct SYDEQuestAbilites::SQA_None = AbilityStruct(Ability_None, "N/A", "");
+AbilityStruct SYDEQuestAbilites::SQA_Hasten = AbilityStruct(Ability_Hasten, "Hasten", "");
+AbilityStruct SYDEQuestAbilites::SQA_ChainSmoker = AbilityStruct(Ability_ChainSmoker, "Chain Smoker", "");
+AbilityStruct SYDEQuestAbilites::SQA_Immunity = AbilityStruct(Ability_Immunity, "Immunity", "");
+AbilityStruct SYDEQuestAbilites::SQA_Insomnia = AbilityStruct(Ability_Insomnia, "Insomnia", "");
+AbilityStruct SYDEQuestAbilites::SQA_NaturalBoost = AbilityStruct(Ability_NaturalBoost, "Natural Boost", "");
+AbilityStruct SYDEQuestAbilites::SQA_ConstantStruggle = AbilityStruct(Ability_Constant_Struggle, "Constant Struggle", "");
--- SYDERogueLike/Abilities.h
+++ SYDERogueLike/Abilities.h
@@ -32,11 +32,3 @@ public:
 	static AbilityStruct SQA_NaturalBoost;
 	static AbilityStruct SQA_ConstantStruggle;
 };
-
-AbilityStruct SYDEQuestAbilites::SQA_None = AbilityStruct(Ability_None, "N/A", "");
-AbilityStruct SYDEQuestAbilites::SQA_Hasten = AbilityStruct(Ability_Hasten, "Hasten", "");
-AbilityStruct SYDEQuestAbilites::SQA_ChainSmoker = AbilityStruct(Ability_ChainSmoker, "Chain Smoker", "");
-AbilityStruct SYDEQuestAbilites::SQA_Immunity = AbilityStruct(Ability_Immunity, "Immunity", "");
-AbilityStruct SYDEQuestAbilites::SQA_Insomnia = AbilityStruct(Ability_Insomnia, "Insomnia", "");
-AbilityStruct SYDEQuestAbilites::SQA_NaturalBoost = AbilityStruct(Ability_NaturalBoost, "Natural Boost", "");
-AbilityStruct SYDEQuestAbilites::SQA_ConstantStruggle = AbilityStruct(Ability_Constant_Struggle, "Constant Struggle", "");
